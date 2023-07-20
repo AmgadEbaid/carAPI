7bf1ae92ba65eecedba0db0d9d59a93b7155684e
AmgadEbaid <95368631+AmgadEbaid@users.noreply.github.com>
2023-07-20 02:56:14 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  await app.listen(3000);
+await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
